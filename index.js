73fdeaf1f598bdd4ac930b28eb3f3e7aaf197db3
58873 <mohammad4sure@gmail.com>
2020-04-27 22:41:54 +1000

    Update index.js

--- index.js
+++ index.js
@@ -2,24 +2,21 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const config = require('./config.json');
 const Enmap = require("enmap");
+const fs = require("fs")
 
+bot.on('ready', () => {
+  console.log(`Logged in as ${client.user.username}!`);
+})
 bot.commands = new Enmap();
 
 fs.readdir("./commands/", (err, files) => {
   if (err) return console.error(err);
   files.forEach(file => {
     if (!file.endsWith(".js")) return;
-    // Load the command file itself
     let props = require(`./commands/${file}`);
-    // Get just the command name from the file name
     let commandName = file.split(".")[0];
     console.log(`Attempting to load command ${commandName}`);
-    // Here we simply store the whole thing in the command Enmap. We're not running it right now.
     bot.commands.set(commandName, props);
   });
 });
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.username}!`);
-});
-
 bot.login(config.token);
